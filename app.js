eced82554a645c0594680ea96d10396ee971b0ee
redirectjl1 <152915362+redirectjl1@users.noreply.github.com>
2023-12-04 22:32:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const app = express()
-
+const express = require('express');
+const { Client } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { Client } = require('pg')
+const app = express();
 const {
     DB_USER,
     HOST,
@@ -23,14 +22,13 @@ const client = new Client({
     password: PASSWORD,
     port: DB_PORT,
     ssl: true
-})
+});
 
 const qtd = MAX_CLICKS_PER_LINK;
 const default_url = DEFAULT_URL;
 
 const linear_order = 'SELECT * FROM links WHERE clicks < $1 ORDER BY id ASC LIMIT 1;';
 const random_order = 'SELECT id, link, clicks FROM links WHERE clicks < $1 ORDER BY clicks ASC LIMIT 1;';
-//coment1
 
 client.connect(async function (err) {
     if (err) throw err;
@@ -39,62 +37,83 @@ client.connect(async function (err) {
     // REDIRECT
     app.get('/visit', async (req, res) => {
         const { type } = req.query;
-        const { rows } = await client.query(type == 'random' ? random_order : linear_order, [qtd])
-        //const { rows } = await client.query(random_order, [qtd])
-        //const { rows } = await client.query(linear_order, [qtd])
-        console.log(rows);
-        if (!rows.length) return res.redirect(default_url);
-        await client.query('UPDATE links SET clicks=$2 WHERE link=$1', [rows[0].link, rows[0].clicks + 1])
-        return res.redirect(rows[0].link)
+
+        // Use a transaction to ensure atomicity
+        const client = await client.connect();
+        try {
+            // Begin the transaction
+            await client.query('BEGIN');
+
+            // Select a link to redirect based on the type
+            const { rows } = await client.query(type == 'random' ? random_order : linear_order, [qtd]);
+
+            console.log(rows);
+
+            if (!rows.length) {
+                // No available links, redirect to default_url
+                return res.redirect(default_url);
+            }
+
+            // Update the clicks for the selected link
+            await client.query('UPDATE links SET clicks = $2 WHERE link = $1', [rows[0].link, rows[0].clicks + 1]);
+
+            // Commit the transaction
+            await client.query('COMMIT');
+
+            return res.redirect(rows[0].link);
+        } catch (error) {
+            // Rollback the transaction in case of an error
+            await client.query('ROLLBACK');
+            throw error;
+        } finally {
+            // Release the client back to the pool
+            client.release();
+        }
     });
 
     app.get('/', async (req, res) => {
         const { type } = req.query;
-        const { rows } = await client.query(type == 'random' ? random_order : linear_order, [qtd])
-        //const { rows } = await client.query(random_order, [qtd])
-        //const { rows } = await client.query(linear_order, [qtd])
+        const { rows } = await client.query(type == 'random' ? random_order : linear_order, [qtd]);
         return res.json({
             count: rows.length,
-            available_links: rows.map(r => (r.link))
-        })
-    })
-
+            available_links: rows.map(r => r.link)
+        });
+    });
 
     // Show All Links and counted clicks
     app.get('/links', async (req, res) => {
-        const { rows } = await client.query('SELECT * FROM links;')
-        return res.json(rows)
-    })
+        const { rows } = await client.query('SELECT * FROM links;');
+        return res.json(rows);
+    });
 
     // ADD MORE LINKS
     app.get('/add', async (req, res) => {
         const { link } = req.query;
         try {
-            if(!link) res.redirect(`/links`)
-            await client.query('INSERT INTO links(link, clicks) values ($1, 0);', [link])
+            if (!link) res.redirect(`/links`);
+            await client.query('INSERT INTO links(link, clicks) values ($1, 0);', [link]);
         } catch (error) {}
-        return res.redirect(`/links`)
+        return res.redirect(`/links`);
     });
 
     // Remove link
     app.get('/remove', async (req, res) => {
         const { link } = req.query;
         try {
-            await client.query('DELETE FROM links WHERE link = $1', [link])
+            await client.query('DELETE FROM links WHERE link = $1', [link]);
         } catch (error) {}
-        return res.redirect(301, `/links`)
+        return res.redirect(301, `/links`);
     });
 
-    // 
+    // Reset clicks
     app.get('/reset', async (req, res) => {
-        const { link } = req.query;
         try {
-            await client.query('UPDATE links SET clicks = 0 WHERE TRUE;')
+            await client.query('UPDATE links SET clicks = 0 WHERE TRUE;');
         } catch (error) {}
-        return res.redirect(301, `/links`)
-    })
+        return res.redirect(301, `/links`);
+    });
 
     app.listen(PORT, () => {
-        console.log(`Example app listening on port ${PORT}`)
-    })
+        console.log(`Example app listening on port ${PORT}`);
+    });
 });
